d3acb36e6acce0b9c3f0d7a202c76a45e324bbf9
mcelearr <rorymcelearney@gmail.com>
2016-03-28 12:53:33 +0100

    add cross which changes colour to button

--- skillsTracker.js
+++ skillsTracker.js
@@ -1,9 +1,8 @@
-function change() {
-    var button = document.getElementById("button");
-    alert("Hello! I am an alert box!");
-    RadarChart(".radarChart", data, radarChartOptions);
-}
-
 function toggler(divId) {
     $("#" + divId).toggleClass("hidden");
 }
+
+$('#button-toggle').click(function(){
+  $(this).find('span').toggleClass('glyphicon-ok').toggleClass('glyphicon-remove');
+  RadarChart(".radarChart", data, radarChartOptions);
+});
