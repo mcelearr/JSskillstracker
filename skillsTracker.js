6a04f501d434e1aeb47579bb21e40ebd59912bef
mcelearr <rorymcelearney@gmail.com>
2016-03-28 14:27:38 +0100

    enable dynamic graph changes

--- skillsTracker.js
+++ skillsTracker.js
@@ -4,5 +4,27 @@ function toggler(divId) {
 
 $('.btn').click(function(){
   $(this).find('span').toggleClass('glyphicon-ok').toggleClass('glyphicon-remove');
+  var id = $(this).attr('id');
+  toggleSubskill(id)
+
+  function toggleSubskill(id){
+
+    for (var i = 0; i < subskills.length; i++){
+      if (subskills[i].button == id && subskills[i].status == 0){
+        subskills[i].status = 1;
+      } else if (subskills[i].button == id && subskills[i].status == 1){
+        subskills[i].status = 0;
+      }
+    }
+  }
+
+  var data = [
+        [
+        {axis:skills[0],value:getAverage(skills[0])},
+        {axis:skills[1],value:getAverage(skills[1])},
+        {axis:skills[2],value:getAverage(skills[2])}
+      ]
+      ];
+
   RadarChart(".radarChart", data, radarChartOptions);
 });
