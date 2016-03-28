86968c8fb6a21f5a41062e805f6c827487832d7d
mcelearr <rorymcelearney@gmail.com>
2016-03-28 13:33:09 +0100

    Fix to change target from id to btn class

--- skillsTracker.js
+++ skillsTracker.js
@@ -2,7 +2,7 @@ function toggler(divId) {
     $("#" + divId).toggleClass("hidden");
 }
 
-$('#button-toggle').click(function(){
+$('.btn').click(function(){
   $(this).find('span').toggleClass('glyphicon-ok').toggleClass('glyphicon-remove');
   RadarChart(".radarChart", data, radarChartOptions);
 });
