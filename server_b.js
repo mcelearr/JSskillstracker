c172632b0a79558fbacce2fd49a283a2a8113b39
Andrew Couture <couture.andrew@gmail.com>
2016-03-30 15:33:17 +0100

    updated server for testing with github oauth

--- server_b.js
+++ server_b.js
@@ -15,6 +15,17 @@ var mimes = {
   "mp4": "video/mp4"
 }
 var server = http.createServer(function (request, response){
+  var headers = request.headers;
+  var methods = request.method;
+  var body = [];
+  request.on('error', function(err){
+    console.log(err);
+  }).on('data', function(chunk){
+    body.push(chunk);
+  }).on('end', function(){
+    body = Buffer.concat(body).toString();
+  })
+  //console.log(request.headers);
   var serveFile = url.parse(request.url).pathname.slice(1);
   if (serveFile.length<1) serveFile = 'index.html';
   var contentType = mimes[url.parse(request.url).pathname.split('.')[1]];
@@ -24,7 +35,17 @@ var server = http.createServer(function (request, response){
       response.writeHead(404, {'Content-Type': 'text/plain'});
       response.end('404 file not found: '+serveFile);
     }
+    else if (url.parse(request.url).query){
+      var q = url.parse(request.url).query.split('&').map((el)=> el.substring(el.indexOf('=')+1));
+      if (q[0]=='login'){
+        response.writeHead(301, {Location: 'https://github.com/login/oauth/authorize','client_id':'//redacted',
+                                'redirect_uri':'https://www.coudrew.ca/'});
+        response.end();
+      };
+      console.log(q);
+    }
     else {
+      console.log(response.headers);
       response.writeHead(200, {'Content-Type': contentType});
       response.end(fs.readFileSync(serveFile));
     };
@@ -32,5 +53,4 @@ var server = http.createServer(function (request, response){
 });
 
 server.listen(port);
-
 console.log('Server is running at http://localhost:' + port);
