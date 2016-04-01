1f852f830b5cfda8f8c1f3166b38a0ca192796b9
Andrew Couture <couture.andrew@gmail.com>
2016-04-01 13:43:36 +0100

    took out some bloat from server_b

--- server_b.js
+++ server_b.js
@@ -1,8 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-var reqIP = require("request-ip");
-var port = 80;
+var port = 8000;
 var mimes = {
   "html": "text/html",
   "css": "text/css",
@@ -14,24 +13,18 @@ var mimes = {
   "svg": "image/svg+xml",
   "mp4a": "audio/mp4",
   "mp4": "video/mp4"
-}
+};
+var controlledFolders = ['app'];
 var server = http.createServer(function (request, response){
-  var headers = request.headers;
-  var methods = request.method;
-  var body = [];
-  var clIP = reqIP.getClientIp(request);
-  console.log(clIP);
-  request.on('error', function(err){
-    console.log(err);
-  }).on('data', function(chunk){
-    body.push(chunk);
-  }).on('end', function(){
-    body = Buffer.concat(body).toString();
-  })
-  //console.log(request.headers);
+
   var serveFile = url.parse(request.url).pathname.slice(1);
   if (serveFile.length<1) serveFile = 'index.html';
   var contentType = mimes[url.parse(request.url).pathname.split('.')[1]];
+  console.log(request.headers.referer);
+  if (request.method=='TRACE'||request.method=='OPTIONS'){
+    response.writeHead(403, {'Content-Type':'text/plain'});
+    response.end('Method Disallowed: '+request.method);
+  };
 
   fs.stat(serveFile, function(err, stats){
     if (err){
@@ -41,28 +34,16 @@ var server = http.createServer(function (request, response){
     else if (url.parse(request.url).query){
       var q = url.parse(request.url).query.split('&').map((el)=> el.substring(el.indexOf('=')+1));
       if (q[0]=='login'){
-        /*var req = http.request({
-          'protocol':'https',
-          'host':'github.com',
-          'path':'/login/oauth/authorize',
-          'connection':'keep-alive',
-          'headers':{'client_id':'66afe030254d5293c677'}
-        },(res)=>{res.writeHead(301,{Location:'https://github.com/login/oauth'})}*/
         response.writeHead(301, {Location: 'https://github.com/login/oauth/authorize?client_id=44bca640b9dc9b0560bc'});
         response.end();
-
-
-        //response.writeHead(301,{Location:'https://github.com/login/oauth/authorize?client_id=66afe030254d5293c677'});
-        //response.end();
       };
-      //console.log(q);
     }
     else {
-      //console.log(response.headers);
       response.writeHead(200, {'Content-Type': contentType});
       response.end(fs.readFileSync(serveFile));
     };
   });
+
 });
 
 server.listen(port);
