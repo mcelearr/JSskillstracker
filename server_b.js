f450e2a126fdb4f62f6ddf2d239dff9d664413bf
Andrew Couture <couture.andrew@gmail.com>
2016-04-03 17:23:52 +0100

    added app/authorize/auth.html to serve as a loading screen, not quite ready yet. made some changes to server_b.js to handle requests for protected folders. implemented request to github for token, will have to test it in the wild on heroku before continuing. added functionality to accept command line arguments for entering client id and secret, host, and port number. this was to deal with the inability to edit files on a heroku dyno, the inadvisablity of posting client id/secret in an open repo, and just to make it more flexible for future use.

--- server_b.js
+++ server_b.js
@@ -1,7 +1,24 @@
+//helpers
+function setState(){
+  var strOpts = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var str = '';
+  while (str.length<=32){
+    str += strOpts[Math.floor(Math.random()*62)];
+  }
+  return str;
+}
+Array.prototype.queryObj = function(){
+  var o = {};
+  this.forEach(function(el) {if (el.split('=').length>1&&el.split('=').length<3){o[el.split('=')[0]] = el.split('=')[1]}});
+  return o;
+}
+//required flags: --harmony_array_includes
+//host, port, client_id, and client_secret must be passed as last 4 arguments when launched
+var settings = process.argv.slice(process.argv.length-4).queryObj();
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-var port = 8000;
+var port = settings.port;
 var mimes = {
   "html": "text/html",
   "css": "text/css",
@@ -14,37 +31,66 @@ var mimes = {
   "mp4a": "audio/mp4",
   "mp4": "video/mp4"
 };
-var controlledFolders = ['app'];
+var state = '', client_id = settings.client_id, client_secret = settings.client_secret;
+var protectedFiles = ['test.html','app'];
+var acceptedHosts = [settings.host,'github.com'];
 var server = http.createServer(function (request, response){
 
-  var serveFile = url.parse(request.url).pathname.slice(1);
-  if (serveFile.length<1) serveFile = 'index.html';
-  var contentType = mimes[url.parse(request.url).pathname.split('.')[1]];
-  console.log(request.headers.referer);
+  var serveFile = url.parse(request.url).pathname.slice(1); //file to serve derived from url
+  if (serveFile.length<=1) serveFile = 'index.html'; //requests of '/' will default to index
+  var contentType = mimes[url.parse(request.url).pathname.split('.')[1]]; //url checked against mimes to find content type
+
+  //do not allow requests with TRACE or OPTIONS method
   if (request.method=='TRACE'||request.method=='OPTIONS'){
     response.writeHead(403, {'Content-Type':'text/plain'});
     response.end('Method Disallowed: '+request.method);
   };
-
+  //check against protectedFiles array to stop hotlinking or wget'ing of protected resources
+  if (protectedFiles.includes(serveFile.split('/')[0]) && (!request.headers.referer||!acceptedHosts.includes(request.headers.host))){
+    response.writeHead(403, {'Content-Type':'text/plain'});
+    response.end('403 forbidden');
+  }
+  //check that file to serve exists
   fs.stat(serveFile, function(err, stats){
     if (err){
       response.writeHead(404, {'Content-Type': 'text/plain'});
       response.end('404 file not found: '+serveFile);
     }
-    else if (url.parse(request.url).query){
-      var q = url.parse(request.url).query.split('&').map((el)=> el.substring(el.indexOf('=')+1));
-      if (q[0]=='login'){
-        response.writeHead(301, {Location: 'https://github.com/login/oauth/authorize?client_id=44bca640b9dc9b0560bc'});
-        response.end();
-      };
-    }
+    //serve a file
     else {
+      if (url.parse(request.url).query){//check for queries in request and handle them. does this need it's own elseif?
+        var q = url.parse(request.url).query.split('&').queryObj();
+        if (q.login&&q.login==1){
+          state = setState();
+          response.writeHead(301, {Location: 'https://github.com/login/oauth/authorize?client_id='+client_id+'&scope=user&state='+state});
+          response.end();
+        }
+        if (q.code&&q.state==state){
+          var post_options = {
+            protocol: 'https',
+            host: 'github.com',
+            path: '/login/oauth/access_token?client_id='+client_id+'&client_secret='+client_secret+'&code='+q.code+'&state='+state,
+            method: 'POST'
+          };
+          var auth;
+          var auth_post = http.request(post_options, function(res){
+            res.on('data', function(chunk){
+              auth = chunk;
+            });
+          });
+          auth_post.write();
+          auth_post.end();
+          console.log(auth);
+        };
+        };
       response.writeHead(200, {'Content-Type': contentType});
       response.end(fs.readFileSync(serveFile));
     };
   });
 
 });
+//create unguessable string for github oauth transaction
 
+//go
 server.listen(port);
 console.log('Server is running at http://localhost:' + port);
