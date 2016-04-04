e9764b2b68dab8c2755fa3731f17ab861c000494
Andrew Couture <couture.andrew@gmail.com>
2016-04-04 09:59:56 +0100

    tweaking

--- server_b.js
+++ server_b.js
@@ -80,8 +80,6 @@ var server = http.createServer(function (request, response){
           });
           auth_post.write();
           auth_post.end();
-          response.writeHead(200, {'Content-Type': 'text/html'});
-          response.end(fs.readFileSync('/app/authorize/auth.html'));
           console.log(auth);
         };
         };
