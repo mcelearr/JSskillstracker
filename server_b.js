1bf42af8253e92ed70d3c699767149e5e9e75f67
Andrew Couture <couture.andrew@gmail.com>
2016-04-05 15:39:45 +0100

    may have it this time

--- server_b.js
+++ server_b.js
@@ -16,8 +16,8 @@ function getUser(token){
   });
   get_user.setHeader('authorization', token);
   get_user.setHeader('User-Agent', 'jskt');
-  //get_user.end();
-  console.log(get_user);
+  get_user.end();
+  //console.log(get_user);
 };
 //helpers
 Array.prototype.queryObj = function(){
