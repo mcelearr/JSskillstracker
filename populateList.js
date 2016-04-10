b6973b4899cf0157a62d384ee68747973c7a370b
mcelearr <rorymcelearney@gmail.com>
2016-04-10 08:55:48 +0100

    refactor all default content to come from populateList.js

--- populateList.js
+++ populateList.js
@@ -1,10 +1,15 @@
- var ul = document.getElementById('content1one')
+
 
 var text = ['Lorem ipsum dolor sit amet','consectetur adipisicing elit','ut labore et dolore magna aliqua','Lorem ipsum dolor sit amet, consectetur adipisicing elit','sed do eiusmod tempor incididunt ut labore','consectetur adipisicing elit']
 
-for (var i = 0; i < 6; i++){
-  var li = document.createElement('li');
-  var node = document.createTextNode(text[i]);
-  li.appendChild(node);
-  ul.appendChild(li);
+for (var j = 1; j < 10; j++){
+  var content = 'content'+j;
+  console.log(content)
+  var ul = document.getElementById(content);
+    for (var i = 0; i < 6; i++){
+      var li = document.createElement('li');
+      var node = document.createTextNode(text[i]);
+      li.appendChild(node);
+      ul.appendChild(li);
+    }
 }
