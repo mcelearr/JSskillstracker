e226ca040d23216dc15a5a53ea003a466f3c7e97
shouston3 <shouston3@me.com>
2016-04-10 21:37:57 +0100

    further formatting and laying out of the dynamic dom manipulation, also included the solution to the challenge not using the map method instead of a for loop

--- setUpModals.js
+++ setUpModals.js
@@ -1,25 +1,25 @@
 var types = ['Numbers', 'Strings', 'Booleans', 'Arrays', 'Undefined and null', 'Objects', 'Arithmetic operators', 'Logical operators', 'Other operators']
 
 types.map(t => {
-  var nodeModal = document.createElement('div')
-  nodeModal.id = 'modal' + types.indexOf(t)
-  nodeModal.className = 'modal modal-fixed-footer'
-  var nodeContent = document.createElement('div')
-  nodeContent.className = 'modal-content'
   var h4 = document.createElement('h4')
-  h4.innerHTML = t
-  nodeContent.appendChild(h4)
+      h4.innerHTML = t
   var ul = document.createElement('ul')
-  ul.id = 'content' + types.indexOf(t)
-  nodeContent.appendChild(ul)
-  nodeModal.appendChild(nodeContent)
-  var nodeFooter = document.createElement('div')
-  nodeFooter.className = 'modal-footer'
+      ul.id = 'content' + types.indexOf(t)
+  var nodeContent = document.createElement('div')
+      nodeContent.className = 'modal-content'
+      nodeContent.appendChild(h4)
+      nodeContent.appendChild(ul)
   var a = document.createElement('a')
-  a.href = '#!'
-  a.className = 'modal-action modal-close waves-effect waves-green btn-flat'
-  a.innerHTML = 'Back'
-  nodeFooter.appendChild(a)
-  nodeModal.appendChild(nodeFooter)
+      a.href = '#!'
+      a.className = 'modal-action modal-close waves-effect waves-green btn-flat'
+      a.innerHTML = 'Back'
+  var nodeFooter = document.createElement('div')
+      nodeFooter.className = 'modal-footer'
+      nodeFooter.appendChild(a)
+  var nodeModal = document.createElement('div')
+      nodeModal.id = 'modal' + types.indexOf(t)
+      nodeModal.className = 'modal modal-fixed-footer'
+      nodeModal.appendChild(nodeContent)
+      nodeModal.appendChild(nodeFooter)
   document.getElementById('modals').appendChild(nodeModal)
 })
