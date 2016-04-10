2f01ced7b53b82f544ff2a7a1cda0271c263622c
mcelearr <rorymcelearney@gmail.com>
2016-04-10 08:56:34 +0100

    small change to populateList.js

--- populateList.js
+++ populateList.js
@@ -1,10 +1,7 @@
-
-
 var text = ['Lorem ipsum dolor sit amet','consectetur adipisicing elit','ut labore et dolore magna aliqua','Lorem ipsum dolor sit amet, consectetur adipisicing elit','sed do eiusmod tempor incididunt ut labore','consectetur adipisicing elit']
 
 for (var j = 1; j < 10; j++){
   var content = 'content'+j;
-  console.log(content)
   var ul = document.getElementById(content);
     for (var i = 0; i < 6; i++){
       var li = document.createElement('li');
