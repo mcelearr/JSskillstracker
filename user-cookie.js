9a82b1a9da4097c667dd301f5ae90cbf47144d9a
Andrew Couture <couture.andrew@gmail.com>
2016-04-14 11:58:24 +0100

    hooked user object up to the interface, made a few other fixes

--- user-cookie.js
+++ user-cookie.js
@@ -1,27 +1,49 @@
+
 function User(){
   var obj = JSON.parse(document.cookie.split('=')[1]);
   for (prop in obj){
     this[prop] = obj[prop];
-  }
-  User.prototype.setValue = function(property, value){
-    this[property] = value;
-    document.cookie = '';
+  };
+  User.prototype.incValue = function(skill, subSkill){
+    let curSkill = this[skill];
+    if (curSkill[subSkill] < 1){
+      curSkill[subSkill] = 1;
+    }
     document.cookie = 'user='+JSON.stringify(this);
-  }
-}
+  };
+  User.prototype.getAverage = function(skill){
+    let sum = 0, count = 0;
+    for (prop in skill){
+      if (prop != 'title'){
+        sum += skill[prop];
+        count++;
+      };
+    };
+    return sum / count;
+  };
+};
 
 function setCookie(){
   if (!document.cookie) {
   let user = {
-    numbers: 0,
-    string: 0,
-    booleans: 0,
-    arrays: 0,
-    objects: 0,
-    undefinedAndNull: 0,
-    arithmeticOperators: 0,
-    logicalOperators: 0,
-    otherOperators: 0,
+    basicDataTypes: {
+      title: "Basic Data Types",
+      numbers: 0,
+      strings: 0,
+      booleans: 0
+    },
+    advancedDataTypes: {
+      title: "Advanced Data Types",
+      arrays: 0,
+      objects: 0,
+      undefinedAndNull: 0
+    },
+    operators: {
+      title: "Operators",
+      arithmeticOperators: 0,
+      logicalOperators: 0,
+      otherOperators: 0
+    }
   };
   var cookieStr = JSON.stringify(user);
   document.cookie = 'user='+cookieStr;
