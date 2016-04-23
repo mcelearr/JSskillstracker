0fa0f2720cf60e496a671b4d31cdd71919e68ed9
mcelearr <rorymcelearney@gmail.com>
2016-04-23 17:24:31 +0100

    add content for numbers

--- dataBase.js
+++ dataBase.js
@@ -1,1 +1,28 @@
 //TO Do
+
+
+var content = [
+{
+title: "Codecademy arithmetic tutorial",
+contentType: "tutorial",
+URL: "https://www.codecademy.com/en/courses/getting-started-v2/0/3?curriculum_id=506324b3a7dffd00020bf661",
+description: "Basic arithmetic with Codecademy"
+skill: skills[0][0],
+subskill: skills[0][1]
+},
+{
+title: "Numbers docs",
+contentType: "documentation",
+URL: "http://www.w3schools.com/js/js_numbers.asp",
+description: "Documentation for numbers on W3 Schools"
+skill: skills[0][0],
+subskill: skills[0][2]
+},{
+title: "Formatting decimal places",
+contentType: "kata",
+URL: "http://www.codewars.com/kata/formatting-decimal-places-number-0",
+description: "Codewars kata on numbers and formatting decimal places"
+skill: skills[0][0],
+subskill: skills[0][3]
+}
+]
