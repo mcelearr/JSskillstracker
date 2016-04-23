3f7f47418a0b45140d7aa6aa3ed54e3b878901a0
mcelearr <rorymcelearney@gmail.com>
2016-04-23 17:47:50 +0100

    add content for booleans

--- dataBase.js
+++ dataBase.js
@@ -5,7 +5,7 @@ var content = [
 {
 title: "Codecademy arithmetic tutorial",
 contentType: "tutorial",
-URL: "https://www.codecademy.com/en/courses/getting-started-v2/0/3?curriculum_id=506324b3a7dffd00020bf661",
+URL: "https://www.codecademy.com/en/courses/getting-started-v2/0/3",
 description: "Basic arithmetic with Codecademy"
 skill: skills[0][0],
 subskill: skills[0][1]
@@ -16,36 +16,59 @@ contentType: "documentation",
 URL: "http://www.w3schools.com/js/js_numbers.asp",
 description: "Documentation for numbers on W3 Schools"
 skill: skills[0][0],
-subskill: skills[0][2]
+subskill: skills[0][1]
 },{
 title: "Formatting decimal places",
 contentType: "kata",
 URL: "http://www.codewars.com/kata/formatting-decimal-places-number-0",
 description: "Codewars kata on numbers and formatting decimal places"
 skill: skills[0][0],
-subskill: skills[0][3]
+subskill: skills[0][1]
 },
 {
 title: "Codecademy numbers and strings tutorial",
 contentType: "tutorial",
-URL: "https://www.codecademy.com/en/courses/getting-started-v2/1/5?curriculum_id=506324b3a7dffd00020bf661",
+URL: "https://www.codecademy.com/en/courses/getting-started-v2/1/5",
 description: "Basic data types with Codecademy. Learn to access the length property of a string"
-skill: skills[1][0],
-subskill: skills[1][1]
+skill: skills[0][0],
+subskill: skills[0][2]
 },
 {
 title: "Strings docs",
 contentType: "documentation",
 URL: "http://www.w3schools.com/js/js_strings.asp",
 description: "Documentation for strings on W3 Schools"
-skill: skills[1][0],
-subskill: skills[1][2]
+skill: skills[0][0],
+subskill: skills[0][2]
 },{
 title: "repeatIt",
 contentType: "kata",
 URL: "http://www.codewars.com/kata/repeatit",
-description: "Codewars kata to ceate a function called repeatIt that takes a string and a number (n) and repeats the string n times"
-skill: skills[1][0],
-subskill: skills[1][3]
+description: "Codewars kata to create a function called repeatIt that takes a string and a number (n) and repeats the string n times"
+skill: skills[0][0],
+subskill: skills[0][2]
+},
+{
+title: "Codecademy booleans tutorial",
+contentType: "tutorial",
+URL: "https://www.codecademy.com/en/courses/getting-started-v2/1/6",
+description: "Basic data types with Codecademy. Learn to access the length property of a string"
+skill: skills[0][0],
+subskill: skills[0][3]
+},
+{
+title: "Booleans docs",
+contentType: "documentation",
+URL: "http://www.w3schools.com/js/js_booleans.asp",
+description: "Documentation for strings on W3 Schools"
+skill: skills[0][0],
+subskill: skills[0][3]
+},{
+title: "Exclusive 'or' (xor) Logical Operator",
+contentType: "kata",
+URL: "http://www.codewars.com/kata/exclusive-or-xor-logical-operator",
+description: "Codewars kata to create an 'exclusive or' operator that evaluates two booleans and returns true if exactly one of the two expressions is true"
+skill: skills[0][0],
+subskill: skills[0][3]
 }
 ]
