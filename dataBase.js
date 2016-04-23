b9e4189e3c66251583711066eb4b0b6f2a2f498a
mcelearr <rorymcelearney@gmail.com>
2016-04-23 17:34:37 +0100

    add content for strings

--- dataBase.js
+++ dataBase.js
@@ -24,5 +24,28 @@ URL: "http://www.codewars.com/kata/formatting-decimal-places-number-0",
 description: "Codewars kata on numbers and formatting decimal places"
 skill: skills[0][0],
 subskill: skills[0][3]
+},
+{
+title: "Codecademy numbers and strings tutorial",
+contentType: "tutorial",
+URL: "https://www.codecademy.com/en/courses/getting-started-v2/1/5?curriculum_id=506324b3a7dffd00020bf661",
+description: "Basic data types with Codecademy. Learn to access the length property of a string"
+skill: skills[1][0],
+subskill: skills[1][1]
+},
+{
+title: "Strings docs",
+contentType: "documentation",
+URL: "http://www.w3schools.com/js/js_strings.asp",
+description: "Documentation for strings on W3 Schools"
+skill: skills[1][0],
+subskill: skills[1][2]
+},{
+title: "repeatIt",
+contentType: "kata",
+URL: "http://www.codewars.com/kata/repeatit",
+description: "Codewars kata to ceate a function called repeatIt that takes a string and a number (n) and repeats the string n times"
+skill: skills[1][0],
+subskill: skills[1][3]
 }
 ]
